d97ec6ab3b78d522a5d131fb07fb870c032aa961
Efeninyeri <158561609+Efeninyeri@users.noreply.github.com>
2024-12-30 20:10:47 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,34 +6,25 @@ import os
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Bot olarak giriş yaptık: {client.user}')
+    print(f'{bot.user} olarak giriş yaptık')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('!merhaba'):
-        await message.channel.send("Selam!")
-    elif message.content.startswith('!bye'):
-        await message.channel.send("\U0001f642")
-    elif message.content.startswith('!en büyük kim'):
-        await message.channel.send("FERRARİ")
 
-    if message.content.startswith("şifre"):
-        def gen_pass(length=12):
-            """Rastgele bir şifre üretir."""
-            characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()'
-            password = ''.join(random.choice(characters) for _ in range(length))
-            return password
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
 
-        generated_password = gen_pass()
-        await message.channel.send(f"Oluşturulan şifre: {generated_password}")
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
+
+bot.run("token")
 # Çevre değişkeninden tokeni al
 
-client.run("token")
+
